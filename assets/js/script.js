f2095f72853f9af4cb094ca62b6785ee2a1cca8d
gilles <yembitrenaud@gmail.com>
2023-05-04 23:57:40 +0200

    regional

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
+// const activePage = window.location.pathname;
+// const navlinks = document.querySelectorAll('nav .main-nav li a').forEach(link => {
+//     if(link.href.includes(`${activePage}`)){
+//         link.classList.add('active');
+//         console.log(link);
+//     }
+// })
+
+
 const activePage = window.location.pathname;
 const navlinks = document.querySelectorAll('nav .main-nav li a').forEach(link => {
-    if(link.href.includes(`${activePage}`)){
+    if (activePage !== "/" && link.href.includes(`${activePage}`) && !link.classList.contains('active')) {
         link.classList.add('active');
         console.log(link);
     }
-})
+});
